9ab7049adbe30dbc7766c190dcd19be7fed125b4
jumper-hash <kacperskoczen21@gmail.com>
2026-04-20 21:36:16 +0200

    Update file-scanner.sh

--- code-projects/file-scanner/file-scanner.sh
+++ code-projects/file-scanner/file-scanner.sh
@@ -11,13 +11,18 @@ path=$starting_directory;
 fi
 
 cd "$path"
-find "$path" -type f -exec ls -la {} + | grep -v "^total" | while read -r privileges links owner_file group_file rest ; do
-    if [[ "$owner" == '*' || "$owner" == '' ]] && [[ "$group" == '*' || "$group" == '' ]]; then
-        echo "$privileges $links $owner_file $group_file $rest";
-    else
-        if [[ "$owner_file" == "$owner" ]] && [[ "$group_file" == "$group" ]]; then
-            echo -e "$privileges $links $owner_file $group_file $rest \n";
-        fi
+find . -type f -exec ls -la {} + | grep -v "^total" | while read -r privileges links owner_file group_file size month day time rest ; do
+    match_owner=false
+    match_group=false
+
+    if [[ "$owner" == '*' || "$owner" == '' || "$owner_file" == "$owner" ]]; then
+        match_owner=true
+    fi
+    if [[ "$group" == '*' || "$group" == '' || "$group_file" == "$group" ]]; then
+        match_group=true
+    fi
+    if [[ "$match_owner" == true && "$match_group" == true ]]; then
+        echo -e "$privileges $links $owner_file $group_file $size $month $day $time $rest"
     fi
 done
 cd "$starting_directory"
